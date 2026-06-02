9376999aa9b34bad2ca1efb6fbab597c9ab310b2
govind4033 <govindpatel4033@gmail.com>
2026-06-02 15:41:31 +0530

    again fixed import in backend

--- Backend/Middleware/authMiddleware.js
+++ Backend/Middleware/authMiddleware.js
@@ -1,5 +1,5 @@
 const jwt  = require('jsonwebtoken');
-const User = require('../models/User');
+const User = require('../Models/User');
 
 // verification
 exports.protect = async (req, res, next) => {
